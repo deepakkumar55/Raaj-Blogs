5759f93f467c932dd0597ac263a0837331e70a85
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2024-07-04 20:47:27 +0530

    Web Analytics check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Analytics } from "@vercel/analytics/react"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My App</h1>
+      <Analytics id="UA-123456-1" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
